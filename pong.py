48149762ed55c864b861c4668dd73d8c8104656d
beellz <belal20795@gmail.com>
2020-08-11 00:05:18 +0530

    added score funcatnality with numbering

--- pong.py
+++ pong.py
@@ -6,6 +6,13 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+#score 
+score_a = 0
+score_b = 0
+
+
+
+
 #paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -37,9 +44,7 @@ ball.dx = 0.3
 ball.dy = -0.3
 
 
-
 #pen
-
 pen = turtle.Turtle()
 pen.speed(0)
 pen.color("white")
@@ -47,13 +52,6 @@ pen.penup()
 pen.hideturtle()
 pen.goto(0 , 260)
 pen.write("Player A: 0 Player B: 0", align="center", font=("courier", 24 , "normal"))
-
-
-
-
-
-
-
 
 
 #function
@@ -107,10 +105,17 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0 , 0)
         ball.dx *= -1      
-        
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("courier", 24 , "normal"))
+
+
     if ball.xcor() < -390:
         ball.goto(0 , 0)
-        ball.dx *= -1    
+        ball.dx *= -1
+        score_b += 1    
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("courier", 24 , "normal"))
 
     #paddle and ball collision
     if (ball.xcor() > 340 and ball.xcor() < 350 ) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
